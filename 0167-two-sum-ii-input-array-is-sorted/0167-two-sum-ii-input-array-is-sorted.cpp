579d8e633a1e6c923387da2e7348b6e7be5680fd
Yojna Singh <125824975+YoJu310@users.noreply.github.com>
2026-08-02 22:38:43 +0530

    Time: 0 ms (100%), Space: 19.4 MB (95.72%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0;
-        int right = numbers.size() - 1;
+    int binarySearch (vector <int> & numbers, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-        while(left < right) {
+            if (numbers[mid] ==  target)
+                return mid;
+            
+            else if (numbers[mid] < target)
+                start = mid + 1;
 
-            int sum = numbers[left] + numbers[right];
-
-            if(sum == target) {
-                return {left + 1, right + 1};
-            }
-
-            else if(sum < target) {
-                left++;
-            }
-
-            else {
-                right--;
-            }
-        }
-
-    return {};
+            else 
+                end = mid - 1;
         
+        }
+        return -1;
+    }
+
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n = numbers.size();
+
+        for (int i =0 ; i<n ; i++) {
+            int remaining = target - numbers[i];
+             
+            int index =  binarySearch (numbers, i+1 , n-1, remaining);
+
+            if (index != -1){
+                return {i+1, index+1};
+                // i+1, index+1 bcoz arr is 1-indexed
+            }
+
+
+        }
+         
+    return {};   
     }
 };
