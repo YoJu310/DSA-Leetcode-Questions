0e83eb96a31acae0f0f7f03e986c8fd3ab59cf09
Yojna Singh <125824975+YoJu310@users.noreply.github.com>
2026-08-07 23:23:29 +0530

    Time: 19 ms (81.05%), Space: 17.5 MB (56.35%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    // better approach - hashset
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> st;
-        for (int i = 0; i < n ; i++){
-            for (int j = i+1; j < n ; j++){
-                unordered_set<long long> hashset;
-                for ( int k = j+1; k < n ; k++){
-                    long long sum = (long long) nums[i] + nums[j] + nums[k] ;
-                    long long fourth = (long long)target - sum ;
-                    if (hashset.find(fourth) != hashset.end()){
-                        vector<int> temp = { nums[i], nums[j], nums[k], int(fourth)};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                    }
-                    hashset.insert(nums[k]);
-                }
-            }
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
 
+        for(int i = 0 ; i < n; i++){
+            // skip duplicate i
+            if ( i > 0 && nums[i] == nums[i-1])
+                continue;
+            for ( int j = i+1 ; j < n ; j++){
+                 // skip duplicate j
+                if (j != i+1 && nums[j] == nums[j-1])
+                    continue;
+
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right){
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target){
+                        ans.push_back ({nums[i] , nums[j], nums[left], nums[right]});
+                        left++;
+                        right--;
+
+                        // skip duplicate left
+                        while(left < right && nums[left] == nums[left-1])
+                            left++;
+                        
+                        // skip duplicate right
+                        while (left < right && nums[right] == nums[right+1])
+                            right--;
+                    }
+                    else if (sum < target)
+                        left++;
+                    else right--;    
+                }
+
+            }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
-        return ans;    
+        return ans;
     }
 };
